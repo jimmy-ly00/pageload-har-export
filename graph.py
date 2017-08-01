a1a1478ae42522b75317828079cc35f78410264a
Jimmy <jimmy-ly00@users.noreply.github.com>
2017-08-01 04:10:09 +0100

    test script

--- graph.py
+++ graph.py
@@ -2,26 +2,82 @@ import json
 import os
 from haralyzer import HarParser
 import dateutil.parser
+import datetime
+import numpy as np
 import matplotlib.pyplot as plt
 
-data = []
+def scan_files(path):
+    data = []
+    # Parse all files in directory
+    for filename in os.listdir(path):
+        with open(os.path.join(path, filename), 'r') as f:
+            har_parser = HarParser(json.loads(f.read()))
 
-for filename in os.listdir(os.getcwd()):
-    if filename == "graph.py":
-        continue
-    with open(filename, 'r') as f:
-        har_parser = HarParser(json.loads(f.read()))
-	max = len(har_parser.pages[0].entries)
-	start_time = har_parser.pages[0].entries[0]["startedDateTime"]
-	end_time =  har_parser.pages[0].entries[max - 1]["startedDateTime"]
-	end_load = float(har_parser.pages[0].entries[max - 1]["time"]) / 1000
-	diff = dateutil.parser.parse(end_time) - dateutil.parser.parse(start_time)
-	data.append((diff.total_seconds()) + end_load)
+        start_time = dateutil.parser.parse(har_parser.pages[0].entries[0]["startedDateTime"]) 
+        latest_time = start_time
+        
+        # Parse all resources HTML, CSS, JS...
+        for entry in (har_parser.pages[0].entries):
+            if entry["time"] == None:
+                s = 0
+            else:
+                s = float(entry["time"])/1000
 
-sorted_data = sorted(data, key=float, reverse=True)
-num = list(range(1, len(data)+1))
-print(num)
-plt.scatter(num, sorted_data)
-plt.xlabel('Number of experiments')
-plt.ylabel('Page loading times')
-plt.show()
+            current_time = dateutil.parser.parse(entry["startedDateTime"]) + datetime.timedelta(seconds = s)
+            if (current_time > latest_time):
+                latest_time = current_time
+      
+        total = latest_time - start_time
+        #if total > datetime.timedelta(seconds = 100):
+        #    os.remove(os.path.join(path, filename))
+
+        data.append(total.total_seconds())
+    return data
+
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
+
+def main():
+    a = scan_files(os.getcwd() + '/traditionalnodelay1')
+    b = scan_files(os.getcwd() + '/angularnodelay1')
+#    c = scan_files(os.getcwd() + '/inline')
+    
+    sorted_a = sorted(a, key=float, reverse=True)
+    sorted_b = sorted(b, key=float, reverse=True)
+#    sorted_c = sorted(c, key=float, reverse=True)
+    #print(sorted_data)
+    
+    num_a = list(range(1, len(a)+1))
+    num_b = list(range(1, len(b)+1))
+#    num_c = list(range(1, len(b)+1))
+
+    # Plot scatter
+    plt.scatter(num_a, sorted_a, label="Traditional", marker="o", s=12)
+    plt.scatter(num_b, sorted_b, label="Angular", marker="^", s=12)
+#    plt.scatter(num_b, sorted_c, label="Inline", marker="s", s=12)
+    plt.legend(loc='upper right')
+    plt.xlabel('Number of experiments')
+    plt.ylabel('Page loading times (seconds)')
+
+    # Plot histogram
+#    plt.hist(sorted_c, normed=True, facecolor='green', bins=15)
+#    plt.xlabel('Page loding times (seconds)')
+#    plt.ylabel('Frequency')
+    
+    # Plot bar chart
+#    a = scan_files(os.getcwd() + '/traditionalnodelay1')
+#    b = scan_files(os.getcwd() + '/angularnodelay1')
+
+#    objects = ('Traditional', 'AngularJS')
+#    y_pos = np.arange(len(objects))
+#    performance = [mean(a), mean(b)]
+#    plt.bar(y_pos, performance,0.5, align='center', alpha=0.5)
+#    plt.xticks(y_pos, objects)
+#    plt.ylabel('Mean loading times (seconds)')
+
+    plt.show()
+
+if __name__ == "__main__":
+    main()
+
+
